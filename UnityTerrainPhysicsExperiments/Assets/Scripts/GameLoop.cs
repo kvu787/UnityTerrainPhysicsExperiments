549ac5a7ac08d9b5f3b07218b731941c545f516e
Kevin Vu <kevin.m.vu@gmail.com>
2026-04-24 23:25:01 -0700

    Add logic to zoom in/out with gamepad triggers. Refactor logic to adjust internal values before setting transforms.

--- UnityTerrainPhysicsExperiments/Assets/Scripts/GameLoop.cs
+++ UnityTerrainPhysicsExperiments/Assets/Scripts/GameLoop.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -8,14 +9,33 @@ using UnityEngine.SceneManagement;
 /// This means that the player can only tell an object to move in the X or Y directions using a gamepad stick or WASD.
 /// The game systems will set the Z position of each object based on the object's XY position, the object's dimensions, and the terrain.
 ///
-/// Object orientation:
+/// Object rotation:
 /// The front facing vector of the object should never point straight up.
 /// </summary>
 public class GameLoop : MonoBehaviour {
+    [SerializeField]
+    [Range(0f, 100f)]
+    private float CameraPivotTranslationSpeed = 20f;
+
+    [SerializeField]
+    [Range(0f, 100f)]
+    private float CameraZoomSpeed = 100f;
+
+    [SerializeField]
+    private float CameraPivotYawSpeed = 180f;
+
+    [SerializeField]
+    private float CameraPivotPitchSpeed = 100f;
+
     public GameObject Car;
     public GameObject CameraPivot;
-    public Camera MainCamera;
+    public Camera Camera;
     public Collider TerrainCollider;
+
+    private Vector3 CameraPivotPosition = new(-3, 0, 2.5f);
+    private float CameraPivotYaw = 133;
+    private float CameraPivotPitch = 65;
+    private float CameraPositionZ = -22;
 
     void Awake() {
         Debug.Log($"GameLoop Awake on object='{this.gameObject.name}' in scene='{this.gameObject.scene.name}'");
@@ -42,31 +62,36 @@ public class GameLoop : MonoBehaviour {
             return;
         }
 
-        Gamepad gamepad = Gamepad.current;
+        if (Gamepad.current != null) {
+            Gamepad gamepad = Gamepad.current;
+            Vector2 leftStick = gamepad.leftStick.ReadValue();
+            Vector2 rightStick = gamepad.rightStick.ReadValue();
+            float leftTrigger = gamepad.leftTrigger.ReadValue();
+            float rightTrigger = gamepad.rightTrigger.ReadValue();
 
-        if (gamepad == null) {
-            return;
+            // Change camera parameters based on gamepad input
+            this.CameraPivotYaw += Time.deltaTime * this.CameraPivotYawSpeed * rightStick.x;
+            this.CameraPivotPitch += Time.deltaTime * this.CameraPivotPitchSpeed * rightStick.y;
+            this.CameraPivotPosition += Time.deltaTime * this.CameraPivotTranslationSpeed * (Quaternion.Euler(0f, this.CameraPivotYaw, 0f) * new Vector3(leftStick.x, 0, leftStick.y));
+            this.CameraPositionZ += Time.deltaTime * this.CameraZoomSpeed * (rightTrigger - leftTrigger);
+
+            // Set camera transforms
+            this.Camera.transform.localPosition = new Vector3(0, 0, this.CameraPositionZ);
+            this.CameraPivot.transform.SetPositionAndRotation(this.CameraPivotPosition, Quaternion.Euler(this.CameraPivotPitch, this.CameraPivotYaw, 0));
         }
 
-        Vector2 leftStick = gamepad.leftStick.ReadValue();
-        Vector3 cameraPivotTranslation = new(leftStick.x, 0, leftStick.y);
-        cameraPivotTranslation = Quaternion.Euler(0f, this.CameraPivot.transform.eulerAngles.y, 0f) * cameraPivotTranslation;
-        float moveSpeed = 20f;
-        this.CameraPivot.transform.position += moveSpeed * Time.deltaTime * cameraPivotTranslation;
-
-        Vector2 rightStick = gamepad.rightStick.ReadValue();
-        float rotateSpeedDegreesPerSecond = 180f;
-        float yawDelta = rightStick.x * rotateSpeedDegreesPerSecond * Time.deltaTime;
-        this.CameraPivot.transform.Rotate(0, yawDelta, 0, Space.World);
-
-        if (TryGetScreenCenterHitPoint(this.MainCamera, this.TerrainCollider, out Vector3 hitPoint, out Vector3 hitNormal)) {
-            float offset = -135f;
-            Quaternion spin = Quaternion.AngleAxis(this.CameraPivot.transform.eulerAngles.y + offset, Vector3.up);
-            Quaternion align = Quaternion.FromToRotation(Vector3.up, hitNormal);
-            Quaternion carOrientation = align * spin;
-
-            this.Car.transform.SetPositionAndRotation(hitPoint, carOrientation);
+        // Change car transform based on camera transform and terrain collision
+        Vector3 newCarPosition = this.Car.transform.position;
+        bool isHit = TryGetScreenCenterHitPoint(this.Camera, this.TerrainCollider, out Vector3 hitPoint, out Vector3 hitNormal);
+        if (isHit) {
+            newCarPosition = hitPoint;
         }
+        float carYawOffset = -135f;
+        Quaternion newCarRotation = Quaternion.AngleAxis(this.CameraPivotYaw + carYawOffset, Vector3.up);
+        if (isHit) {
+            newCarRotation = Quaternion.FromToRotation(Vector3.up, hitNormal) * newCarRotation;
+        }
+        this.Car.transform.SetPositionAndRotation(newCarPosition, newCarRotation);
     }
 
     // hitPoint is in world space and hitNormal is normalized
